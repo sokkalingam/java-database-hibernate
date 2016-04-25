7fb5bc5386ba89ee3427747392a3bc73b432674d
Sokkalingam SUBRAMANIAN <sokkalingam.subramanian@amadeus.com>
2016-04-24 20:40:10 -0400

    populate run

--- src/test/GemTest.java
+++ src/test/GemTest.java
@@ -1,47 +1,44 @@
 package test;
 
-import java.util.List;
-
-import databases.GemDatabase;
-import databases.ReviewDatabase;
-import models.Gem;
-import models.review.Review;
+import resources.GemResource;
 
 public class GemTest {
 	
 	public static void main(String[] args) {
 		
-		GemDatabase gemDatabase = new GemDatabase();
+//		GemDatabase gemDatabase = new GemDatabase();
+//		
+//		for (int i = 0 ; i < 10; i ++)
+//			gemDatabase.addGem(Gem.generateModel());
+//		
+//		List<Gem> gems = gemDatabase.readGems();
+//		
+//		Gem gem = gems.get(0);
+//		
+//		gem.setName("Boom Man");
+//		
+//		gemDatabase.updateGem(gem);
+//		
+//		gems = gemDatabase.readGems();
+//		
+//		ReviewDatabase reviewDatabase = new ReviewDatabase();
+//		
+//		reviewDatabase.addReview(gem.getId(), new Review(5, "Awesome Review", "Sokka"));
+//		reviewDatabase.addReview(gem.getId(), new Review(1, "Bad Review", "Jon"));
+//		
+//		gems = gemDatabase.readGems();
+//		List<Review> reviews = reviewDatabase.getReviews(gem.getId());
+//		
+//		Review review = reviews.get(0);
+//		
+//		reviewDatabase.deleteReview(gem.getId(), review);
+//		
+//		reviews = reviewDatabase.getReviews(gem.getId());
+//		
+//		gems = gemDatabase.readGems();
+//		
+//		gemDatabase.closeSessionFactory();
 		
-		for (int i = 0 ; i < 10; i ++)
-			gemDatabase.addGem(Gem.generateModel());
-		
-		List<Gem> gems = gemDatabase.readGems();
-		
-		Gem gem = gems.get(0);
-		
-		gem.setName("Boom Man");
-		
-		gemDatabase.updateGem(gem);
-		
-		gems = gemDatabase.readGems();
-		
-		ReviewDatabase reviewDatabase = new ReviewDatabase();
-		
-		reviewDatabase.addReview(gem.getId(), new Review(5, "Awesome Review", "Sokka"));
-		reviewDatabase.addReview(gem.getId(), new Review(1, "Bad Review", "Jon"));
-		
-		gems = gemDatabase.readGems();
-		List<Review> reviews = reviewDatabase.getReviews(gem.getId());
-		
-		Review review = reviews.get(0);
-		
-		reviewDatabase.deleteReview(gem.getId(), review);
-		
-		reviews = reviewDatabase.getReviews(gem.getId());
-		
-		gems = gemDatabase.readGems();
-		
-		gemDatabase.closeSessionFactory();
+		new GemResource().populate(10, 2);
 	}
 }
